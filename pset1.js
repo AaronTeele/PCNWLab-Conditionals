fd0a0cc198b84e466f360928f88143699646e39f
AaronTeele <32844775+AaronTeele@users.noreply.github.com>
2018-10-14 17:27:36 -0400

    completed psets

--- pset1.js
+++ pset1.js
@@ -13,7 +13,13 @@ function generateRandomNumberFromRange( s, e ) {
             
     @example generateTrafficLight(); // 'red' or 'blue' or 'green'
 */
-
+    const generateTrafficLight = () => {
+        const light = generateRandomNumberFromRange(1, 3)
+        if (light === 1) return "red";
+        if (light === 2) return "green";
+        return "yellow";
+    };
+    console.log('Traffic Light:', generateTrafficLight());
 /*
     @func generateRandomPhoneNumber
     @returns {string}
@@ -22,6 +28,51 @@ function generateRandomNumberFromRange( s, e ) {
             
     @example generateRandomPhoneNumber(); // "1-718-786-2825"
 */
+
+    const pad = (phoneNum, digits) => {
+        if(digits === 3){
+            if (num < 10) {
+                return digits = "00" + digits;
+            } else if (phoneNum < 100) {
+              return digits = "0" + digits;
+            };
+        } else if (num === 4){
+            if (digits < 10) {
+                digits = "000" + digits;
+            } else if (digits < 100) {
+                digits = "00" + digits;
+            } else if (digits < 1000){
+                digits = "0" + digits;
+            };
+        }
+    }
+    const generateRandomPhoneNumber = () => {
+        //let phoneNum = `1 - ${generateRandomNumberFromRange(000, 999)} - ${generateRandomNumberFromRange(000, 999)} - ${generateRandomNumberFromRange(1000, 9999)} `;
+        let phoneNum = "1-"
+        let digits1 = generateRandomNumberFromRange(0, 999)
+        let digits2 = generateRandomNumberFromRange(0, 999)
+        let digits3 = generateRandomNumberFromRange(0, 9999)
+        
+        
+
+        if (digits2 < 10) {
+            digits2 = "00" + digits2;
+        } else if (digits2 < 100) {
+            digits2 = "0" + digits1;
+        };
+
+        if (digits3 < 10) {
+            digits3 = "000" + digits3;
+        } else if (digits3 < 100) {
+            digits3 = "00" + digits3;
+        } else if (digits3 < 1000){
+            digits3 = "0" + digits3;
+        };
+
+        return phoneNum += `${digits1}-${digits2}-${digits3}`;
+    };
+    
+    console.log("Random Phone #:", generateRandomPhoneNumber());
 
 /*
     @func generateRandomRGB
@@ -33,6 +84,15 @@ function generateRandomNumberFromRange( s, e ) {
     
     @example generateRandomRGB(); // "rgb(255, 123, 0)"
 */
+
+
+  const generateRandomRGB = () => {
+    let r = generateRandomNumberFromRange(0, 255);
+    let g = generateRandomNumberFromRange(0, 255);
+    let b = generateRandomNumberFromRange(0, 255);
+    return `rgn(${r}, ${g}, ${b})`
+  }
+  console.log("Random RGB:", generateRandomRGB());
 
 /*
     @func generateLottoTicket
@@ -46,7 +106,13 @@ function generateRandomNumberFromRange( s, e ) {
     
     @example generateLottoTicket(); // L 9 11-28
 */
-
+const generateLottoTicket = () => {
+    let a = generateRandomNumberFromRange(0, 9);
+    let b = generateRandomNumberFromRange(0, 15);
+    let c = generateRandomNumberFromRange(0, 30);
+    return `L [${a}] [${b}]-[${c}]`
+}
+    console.log("Generate Lotto Ticket:", generateLottoTicket());
 /*
     @func generatePhoneNumberWithAreaCode
     @param {number} areaCode
@@ -61,6 +127,40 @@ function generateRandomNumberFromRange( s, e ) {
     @example generatePhoneNumberWithAreaCode( 646 ); // "1-646-786-2825"
 */
 
+const generatePhoneNumberWithAreaCode = (areaCode) => {
+   
+     let phoneNum = "1-"
+      
+     let digits2 = generateRandomNumberFromRange(000, 999)
+     let digits3 = generateRandomNumberFromRange(0000, 9999)
+     
+     if (areaCode = undefined) {
+        digits1 = generateRandomNumberFromRange(000, 999)
+     };
+
+     if (digits1 < 10) {
+         digits1 = "00" + digits1;
+     } else if (digits1 < 100) {
+         digits1 = "0" + digits1;
+     };
+
+     if (digits2 < 10) {
+         digits2 = "00" + digits2;
+     } else if (digits2 < 100) {
+         digits2 = "0" + digits1;
+     };
+
+     if (digits3 < 10) {
+         digits3 = "000" + digits3;
+     } else if (digits3 < 100) {
+         digits3 = "00" + digits3;
+     } else if (digits3 < 1000){
+         digits3 = "0" + digits3;
+     };
+
+     return phoneNum += `${areaCode}-${digits2}-${digits3}`;   
+};
+    console.log("Phonne # with area code:", generatePhoneNumberWithAreaCode());
 
 /*
     @func generateTicketWithLetters
@@ -76,7 +176,16 @@ function generateRandomNumberFromRange( s, e ) {
             
         @example generateLottoTicket(); // L g M-28
 */
-
+    const generateTicketWithLetters = () => {
+        let a = generateRandomNumberFromRange(0, 9);
+        let upperCase = String.fromCharCode(generateRandomNumberFromRange(65, 90));
+        let lowerCase = String.fromCharCode(generateRandomNumberFromRange(97, 122));
+        
+        let b = generateRandomNumberFromRange(0, 15);
+        let c = generateRandomNumberFromRange(0, 30);
+        return `L ${lowerCase} ${upperCase}-${c}`
+    }
+    console.log(generateTicketWithLetters())
 /*
     @func rockPaperScissors
     @param {string} player1
@@ -97,6 +206,19 @@ function generateRandomNumberFromRange( s, e ) {
     @example rockPaperScissors( "r" ); // -1
     @example rockPaperScissors(); // -1
 */
+const rockPaperScissors = (p1, p2) => {
+    p1 = p1.toLowerCase()
+    p2 = p2.toLowerCase()
+    if (p1 !== "rock" && p !== "scissors" && p !== "paper" || p !== "rock" && p !== "scissors" && p !== "paper"){
+        return -1
+    } else if (p === p && ) {
+        return 0;
+    } else if ((p === "rock" && p2 === "scissors") || (p === "scissors" &&  p2 === "paper") || (p === "paper" && p2 === "rock")){
+        return 1
+    } else return 2
+}
+rockPaperScissors("rock", "scissors");
+
 
 /*
     @func RPSwithComputer
@@ -113,6 +235,35 @@ function generateRandomNumberFromRange( s, e ) {
     @example rockPaperScissors( "rock" ); // 0, if tied
     @example rockPaperScissors(); // -1
 */
+const RPSwithComputer = player => {
+    
+    //computer makes random choice from the compPicksOne array. computerChoice has to be made inside the function so it makes a new choice everytime.
+    let computerChoice = compPicksOne[Math.floor(Math.random() * 3)];
+    if (computerChoice === 1) {
+        return rockPaperScissors(player, r)
+    }
+    if (computerChoice === 2){
+        return rockPaperScissors(player, p)
+    }
+
+    if (computerChoice === 3){
+        return rockPaperScissors(player, s)
+    }
+
+    //just in case player enters invalid input, prompts with correct choices
+    if(playerChoic !== "rock" && playerChoic !== "scissors" && playerChoic !== "paper"){
+        return -1;
+    } else if (playerChoic === computerChoice){
+        //log player and computer made the same choice
+        return 0;
+    } else if ((playerChoic === "rock" && computerChoice === "scissors") || (playerChoic === "scissors" &&  computerChoice === "paper") || (playerChoic === "paper" && computerChoice === "rock")){
+        //the above scenarios are all in which the player wins (rock vs scissors, scissors vs paper or paper vs rock)        
+        return 1;
+    } else {
+        //log that computers choice beats players choice
+        return 2;
+    };
+}
 
 /*
     @func calculateGrade
@@ -132,24 +283,33 @@ function generateRandomNumberFromRange( s, e ) {
     
 */
 
-function calculateGrade( grade ) {
-    // Write your codes below here
-    /*
-        assume that grade is the value the user input
-        IF grade is between [100, 95]
-            UPDATE grade = A (question: what kind of variable should A be?)
-        if grade is between [95, 85]
-            UPDATE grade = B
-        if grade is between [85, 75]
-            UPDATE grade = C
-        if grade is between [75, 65]
-            UPDATE grade = D
-        if grade is between [65, 0]
-            UPDATE grade = F
-    */
+// function calculateGrade( grade ) {
+//     // Write your codes below here
+//     /*
+//         assume that grade is the value the user input
+//         IF grade is between [100, 95]
+//             UPDATE grade = A (question: what kind of variable should A be?)
+//         if grade is between [95, 85]
+//             UPDATE grade = B
+//         if grade is between [85, 75]
+//             UPDATE grade = C
+//         if grade is between [75, 65]
+//             UPDATE grade = D
+//         if grade is between [65, 0]
+//             UPDATE grade = F
+//     */
     
-    if ( grade > 90 ) {
-        return "A";
-    }
-   return "F"; // you may choose not to use this
-} 
+//     if ( grade > 90 ) {
+//         return "A";
+//     }
+//    return "F"; // you may choose not to use this
+// } 
+
+const calculateGrade = grade => {
+    if (grade <= 100 && grade >= 95) return "A";
+    else if (grade < 95 && grade >= 85 ) return "B";
+    else if (grade < 85 && grade >= 75) return "C";
+    else if (grade < 75 && grade >= 65) return "D";
+    else if (grade < 65 && grade >= 0) return "F";
+    else return "ERROR"
+}
